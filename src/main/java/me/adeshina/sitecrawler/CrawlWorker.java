21a5ffda2c88b8a8c365c93e7b2a48fcd14c9d6a
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2019-09-04 23:32:53 +0100

    prepare release - September 5 2019

--- src/main/java/me/adeshina/sitecrawler/CrawlWorker.java
+++ src/main/java/me/adeshina/sitecrawler/CrawlWorker.java
@@ -1,6 +1,8 @@
 package me.adeshina.sitecrawler;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Optional;
@@ -9,8 +11,12 @@ import java.util.Set;
 import me.adeshina.sitecrawler.dto.WebPage;
 
 import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Core class responsible for actual crawling.
@@ -60,7 +66,15 @@ final class CrawlWorker {
      */
     private WebPage processPage(String url) throws IOException {
 
-        Connection.Response response = Jsoup.connect(url).userAgent(config.userAgent()).execute().bufferUp();
+        Connection.Response response;
+
+        try {
+            response = Jsoup.connect(url).userAgent(config.userAgent()).execute().bufferUp();
+
+        } catch (MalformedURLException | HttpStatusException | UnsupportedMimeTypeException | SocketTimeoutException e) {
+            throw new IOException(e);
+        }
+
         Document page = response.parse();
         clean(page);
         page.select("a[href]").stream().map(link -> link.absUrl("href")).forEach(queue::add);
@@ -79,7 +93,12 @@ final class CrawlWorker {
         Arrays.asList(tags).forEach(tag -> page.body().select(tag).remove());
 
         // clean head
-        Arrays.asList("script", "style").forEach(tag -> page.head().select(tag).remove());
+        Arrays.asList("script", "style").forEach(tag -> {
+            Elements element = page.head().select(tag);
+            if (!element.isEmpty()) {
+                element.remove();
+            }
+        });
     }
 
 }
